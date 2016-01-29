2a8b58d8794ad9dfac5fedfa89285c49d0749d40
Michael Ha <mha.hk.ca@gmail.com>
2016-01-29 12:07:53 -0800

    prototype example

---
+++ 21-prototype/app.js
@@ -0,0 +1,39 @@
+function Person(firstname, lastname) {
+  this.firstname = firstname;
+  this.lastname = lastname;
+}
+
+Person.prototype.getFullName = function() {
+  return this.firstname + ' ' + this.lastname;
+}
+
+var john new Person('John', 'Doe');
+
+Person.prototype.getFormalFullName = function() {
+  return this.lastname + ', ' + this.firstname;
+}
+
+function Person(firstname, lastname) {
+
+    console.log(this);
+    this.firstname = firstname;
+    this.lastname = lastname;
+    console.log('This function is invoked.');
+
+}
+
+Person.prototype.getFullName = function() {
+    return this.firstname + ' ' + this.lastname;
+}
+
+var john = new Person('John', 'Doe');
+console.log(john);
+
+var jane = new Person('Jane', 'Doe');
+console.log(jane);
+
+Person.prototype.getFormalFullName = function() {
+    return this.lastname + ', ' + this.firstname;
+}
+
+console.log(john.getFormalFullName());
